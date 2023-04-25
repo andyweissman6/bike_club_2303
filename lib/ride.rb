2ea8946064b4348fed08bf0bbc35e4f7a7e354ec
Andy Weissman <andyweissman6@gmail.com>
2023-04-25 11:28:28 -0600

    create ride class, #loop, #total_distance methods

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,3 +1,27 @@
 class Ride
+  attr_reader :name,
+              :distance,
+              :terrain,
+              :loop
+
+  def initialize(attributes)
+    @name = attributes[:name]
+    @distance = attributes[:distance]
+    @terrain = attributes[:terrain]
+    @loop = attributes[:loop]
+  end
+
+  def loop?
+    @loop
+  end
+
+  def total_distance
+    if loop? == false
+      @distance * 2
+    else
+      @distance
+    end
+
+  end
 
 end
