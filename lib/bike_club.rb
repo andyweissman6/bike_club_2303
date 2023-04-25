b9e49ef0170de16c86e3d5e968eb5130ac4ae6a9
Andy Weissman <andyweissman6@gmail.com>
2023-04-25 11:58:13 -0600

    create BikeClub class, added add_biker, most_rides methods

--- lib/bike_club.rb
+++ lib/bike_club.rb
@@ -1,3 +1,28 @@
 class BikeClub
+  attr_reader :name,
+              :bikers
+  def initialize(name)
+    @name = name
+    @bikers = []
+  end
+
+  def add_biker(biker)
+    @bikers << biker
+  end
+
+  def most_rides
+    @bikers.find do |biker|
+      biker.rides.count
+    end
+  end
+
+  # def best_time(ride)
+  #   @bikers.find do |biker|
+  #     require 'pry'; binding.pry
+  #     biker.rides.include?(ride)
+  #     # biker.rides.values.sort
+      
+  #   end
+  # end
   
 end
